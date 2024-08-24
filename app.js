8a7e56df040b8e043ee48c84aba100d786968fde
abdunazaarov <abduraximabdunazarov1@gmail.com>
2024-08-25 04:39:19 +0500

    vazifa bajarildi

---
+++ app.js
@@ -0,0 +1,84 @@
+// 1 - Masala
+
+// {
+// let tomon = prompt("Kvadratning tomoni:")
+// console.log(tomon * 4)
+// }
+
+// 2 - Masala
+
+// {
+//     let tomon = prompt("kvadratning tomoni:")
+//     console.log(tomon * tomon)
+// }
+
+// 3 - Masala
+
+// {
+//     let width = prompt("To'rtburchakning eni:")
+//     let height = prompt("To'rtburchakning bo'yi:")
+//     console.log(width * height)
+//     console.log((width + height)*2)
+// }
+
+// 4 - Masala
+
+// {
+//     let aylana = prompt("Aylananing diametri:")
+//     console.log(aylana * 3.14)
+// }
+
+// 5 - Masala
+
+// {
+//     let kub = prompt("Kubning tomoni:")
+//     console.log(kub*kub*kub)
+//     console.log(6 * (kub*kub))
+// }
+
+// 6 - Masala
+
+// {
+//     let a = prompt("A ni kiriting:")
+//     let b = prompt("B ni kiriting:")
+//     let c = prompt("C ni kiriting:")
+//     console.log(a*b*c)
+//     console.log(2*(a*b + b*c + a*c))
+// }
+
+// 7 - Masala
+
+// {
+//     let r = prompt("Aylananing radiusi:")
+//     console.log(2*r*3.14)
+//     console.log(r*r*3.14)
+// }
+
+// 8 - Masala
+
+// {
+//     let a = prompt("A sonini kiriting:")
+//     let b = prompt("B sonini kiriting:")
+//     let result = (a/2 + b/2)
+//     console.log(result)
+// }
+
+// 9 - Masala
+
+// {
+//     let a = Number(prompt('birinchi sonni kiriting:'));
+//     let b = Number(prompt('ikkinchi sonni kiriting:'));
+//     let result = a+b
+//     console.log(Math.sqrt(result));
+// }
+
+// 10 - Masala
+
+// {
+//         let a = Number(prompt('birinchi sonni kiriting:'));
+//         let b = Number(prompt('ikkinchi sonni kiriting:'));
+//         let yigindi = a+b
+//         let kopaytma = a*b
+//         console.log(yigindi, kopaytma, Math.pow(a, 2), Math.pow(b, 2))
+// }
+// ---------------------------------------------------------------------------------------------------------
